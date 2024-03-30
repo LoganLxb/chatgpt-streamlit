3ce5f6d83175172aaa7763a197974d634a8b69d7
LoganLxb <52527895+LoganLxb@users.noreply.github.com>
2024-03-30 14:48:53 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,52 @@
-import random
-import gradio as gr
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, StorageContext, load_index_from_storage
+from llama_index.embeddings.openai import OpenAIEmbedding
+from llama_index.core.node_parser import SentenceSplitter
+from llama_index.llms.openai import OpenAI
+from llama_index.core import Settings
+import gradio
+import os
 
-def alternatingly_agree(message, history):
-    if len(history) % 2 == 0:
-        return f"Yes, I do think that '{message}'"
-    else:
-        return "I don't think so"
+os.environ["OPENAI_API_KEY"] = ''
 
-gr.ChatInterface(alternatingly_agree).launch(share=True)
+def construct_index(directory_path):
+    # set number of output tokens
+    Settings.llm = OpenAI(model="gpt-3.5-turbo")
+    Settings.embed_model = OpenAIEmbedding(model="text-embedding-3-small")
+    Settings.node_parser = SentenceSplitter(chunk_size=512, chunk_overlap=20)
+    Settings.num_output = 512
+    Settings.context_window = 3900
+
+    docs = SimpleDirectoryReader(directory_path).load_data()
+
+    index = VectorStoreIndex.from_documents(docs, embed_model=Settings.embed_model,llm=Settings.llm)
+    
+    #Directory in which the indexes will be stored
+    index.storage_context.persist(persist_dir="indexes")
+
+    return index
+
+def chatbot(input_text):
+    
+    # rebuild storage context
+    storage_context = StorageContext.from_defaults(persist_dir="indexes")
+    
+    #load indexes from directory using storage_context 
+    query_engne = load_index_from_storage(storage_context).as_query_engine()
+    
+    response = query_engne.query(input_text)
+    
+    #returning the response
+    return response.response
+
+#Creating the web UIusing gradio
+iface = gradio.Interface(fn=chatbot,
+                     inputs=gradio.Textbox(lines= 5 , label= "Enter your questions here" ), 
+                     outputs="text",
+                     title="Custom-trained AI Chatbot")
+
+#Constructing indexes based on the documents in traininData folder
+#This can be skipped if you have already trained your app and need to re-run it
+index = construct_index("trainingData")
+
+#launching the web UI using gradio
+iface.launch(share=True)
